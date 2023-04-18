689f9f334b1f75af6be3130dcbddb438c8b07666
amir79 <amir.shahvirdi79@gmail.com>
2023-04-18 12:24:36 +0330

    create authentication 1

--- cinema/views.py
+++ cinema/views.py
@@ -9,3 +9,26 @@ def index(request):
         
     })
 
+def registration(request):
+    if request.method == 'POST':
+        first_name     = request.POST['first_name']
+        last_name      = request.POST['last_name']
+        email          = request.POST['email']
+        password       = request.POST['password']
+        confirmation   = request.POST['confirmation']
+        if confirmation != password:
+            pass
+    
+        else:
+            user = Users.objects.create_user(first_name,last_name,email,password)
+            user.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'cinema/Registration&login.html')
+
+
+def registration_page(request):
+    return render(request, 'cinema/Registration&login.html',{
+
+    })
