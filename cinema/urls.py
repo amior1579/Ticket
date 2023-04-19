4d3a3b401f4ca4788dad48aa993c1a000a375fdc
amir79 <amir.shahvirdi79@gmail.com>
2023-04-19 19:12:47 +0330

    create authentication 1 (add login form)

--- cinema/urls.py
+++ cinema/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('registration', views.registration, name='registration'),
     path('registration_page', views.registration_page, name='registration_page'),
+    path('user_login', views.user_login, name='user_login'),
+    path('login_page', views.login_page, name='login_page'),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.http import HttpResponseRedirect
@@ -28,10 +28,36 @@ def registration(request):
             login(request, user)
             return HttpResponseRedirect(reverse('index'))
     else:
-        return render(request, 'cinema/Registration&login.html')
+        return render(request, 'cinema/Registration.html')
+
 
 
 def registration_page(request):
-    return render(request, 'cinema/Registration&login.html',{
+    return render(request, 'cinema/Registration.html',{
 
+    })
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'cinema/login.html',{
+                'message': 'Invalid username and/or password.',
+            })
+    else:
+        return HttpResponseRedirect(reverse('login_page'))
+
+
+
+def login_page(request):
+    return render(request, 'cinema/login.html',{
+        
     })
